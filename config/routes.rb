172c92cabee9ff2de32dafba0d40681730b3ec2e
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2019-01-10 19:17:46 +0900

    fix devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
 
   resources :users, only: %i(show)
 end
