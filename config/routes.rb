5e5f985811c41a867e04cfe33786fdb7f2824bea
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2019-01-11 00:52:38 +0900

    change home page index action in post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'posts#index'
 
   devise_for :users,
     controllers: { registrations: 'registrations' }
