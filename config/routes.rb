2bf6683a9ebaa240fece9575ace17800a4d80b8d
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2019-01-11 13:19:54 +0900

    add shallow option

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
 
-    resources :likes, only: %i(create destroy)
+    resources :likes, only: %i(create destroy), shallow: true
   end
 end
