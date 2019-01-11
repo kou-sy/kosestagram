9c5b198b10aef801be7bc7b77874e4c235b67a22
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2019-01-11 13:12:50 +0900

    add destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: %i(show destroy)
 
   def index
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
@@ -25,12 +26,26 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find_by(id: params[:id])
+    #@post = Post.find_by(id: params[:id])
     @photos = @post.photos
+  end
+  #@post = Post.find_by(id: params[:id])
+  def destroy
+    if @post.user == current_user
+      flash[:notice] = "Deleted" if @post.destroy
+    else
+      flash[:alert] = "Delete fail"
+    end
+      redirect_to root_path
   end
 
   private
     def post_params
       params.require(:post).permit(:caption).merge(user_id: current_user.id)
     end
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
+    end
+
 end
